70e9dc21736bd3d6ea475e39f1b091b5f6167a52
V win11 <w@example.com>
2024-06-03 18:17:13 +0200

    activate venv

--- our_first_web.py
+++ our_first_web.py
@@ -1,5 +1,5 @@
 from pywebio.input import input as input_pw
-from pywebio.output import put_text
+from pywebio.output import put_text, put_html, put_image
 
 formatted_name = put_text(f'Ура, Літо')
 formatted_name = input_pw(f'Ура, Літо')
@@ -28,3 +28,6 @@ put_text('''Жив собі в лісі зайчисько малий,
 plans = input_pw('Введіть ваші плани на літо:')
 
 put_text(f'Кількість символів у ваших планах на літо:')
+
+img = open('images.jpg', 'rb').read()
+put_image(img, width='500px')

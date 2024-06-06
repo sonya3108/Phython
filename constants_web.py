e996b715291c9dfd63675cabeac63d295a2851cb
V win11 <w@example.com>
2024-06-06 18:55:25 +0200

    activate venv

--- constants_web.py
+++ constants_web.py
@@ -1,2 +1,4 @@
-MSG_INPUT_FIRST_NAME = 'Введіть ваше ім`я: '
-MSG_INPUT_LAST_NAME = 'Введіть ваше прізвище: '
+pizza_cost = 250
+cola_cost = 60
+msg_enter_quantity = 'Введіть бажану кількість'
+msg_ask_name = 'Як вас звати'

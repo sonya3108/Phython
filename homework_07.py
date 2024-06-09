1687459d952b3b8b9ccaad21ecf089e39a3592c7
V win11 <w@example.com>
2024-06-09 15:19:41 +0200

    activate venv

---
+++ homework_07.py
@@ -0,0 +1,9 @@
+def greet_user_with_header_h1(user_name):
+    result = f"<h1>Вітаємо тебе, шановний {user_name}</h1>"
+    return result
+
+
+def get_area_of_rectangle(side_number_1, side_number_2):
+    area_of_rectangle = side_number_1 * side_number_2
+    return area_of_rectangle
+get_area_of_rectangle(side_number_1:3, side_number_2:5)

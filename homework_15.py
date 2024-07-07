e47a6c57ae0bde086d1fcb1f269065b3b8229040
V win11 <w@example.com>
2024-07-07 14:14:17 +0200

    activate venv

--- homework_15.py
+++ homework_15.py
@@ -1,18 +1,34 @@
 import requests
 import json
 
+def download_pdf(url, filename):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(filename, 'wb') as f:
+            f.write(response.content)
+        print(f"PDF файл збережено як {filename}")
+    else:
+        print(f"Не вдалося завантажити файл. Статус код: {response.status_code}")
+
+def fetch_and_save_data(url, filename):
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        with open(filename, 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+        print(f"Дані успішно збережені у файл {filename}")
+    else:
+        print(f"Не вдалося отримати дані. Статус код: {response.status_code}")
+
 
 pdf_url = "https://chtyvo.org.ua/authors/Falkovych_Hryhorii/Smyk-tyndyk.pdf"
-pdf_response = requests.get(pdf_url)
 
-with open("Smyk-tyndyk.pdf", "wb") as pdf_file:
-    pdf_file.write(pdf_response.content)
+pdf_filename = "Smyk-tyndyk.pdf"
 
+download_pdf(pdf_url, pdf_filename)
 
-api_url = "http://api.open-notify.org/astros.json"
-api_response = requests.get(api_url)
-api_data = api_response.json()
+json_url = "http://api.open-notify.org/astros.json"
+json_filename = "astros.json"
 
+fetch_and_save_data(json_url, json_filename)
 
-with open("astros.json", "w") as json_file:
-    json.dump(api_data, json_file, indent=4)
